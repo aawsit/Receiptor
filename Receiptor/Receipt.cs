036a60baf1d3516ecaa9f80220fb41e5ecc91f8e
Christopher Rutherford <therealchrisrutherford@gmail.com>
2018-01-19 11:30:56 -0500

    initial commit

---
+++ Receiptor/Receipt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiptor
+{
+    internal class Contribution
+    {
+        public double amount { get; set; }
+        public string svcNumber { get; set; }
+        public string receiptNumber { get; set; }
+        public string grpName { get; set; }
+        public DateTime ContributionDate { get; set; }
+    }
+
+    internal class Address
+    {
+
+    }
+    class Receipt
+    {
+        public Contribution Contrib { get; set; }
+        
+    }
+}
